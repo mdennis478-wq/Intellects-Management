a7d13584761ddc8365a0b0bde54046cc2ab02628
mdennis478-wq <mdennis478@gmail.com>
2026-06-24 16:33:03 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+const members = [
+  {
+    name: "Jeremiah Mbindyo",
+    savings: 91000
+  },
+  {
+    name: "Dennis Munene",
+    savings: 91000
+  },
+  {
+    name: "Kelvin Macharia",
+    savings: 91000
+  },
+  {
+    name: "Kevin Osumo Simba",
+    savings: 107000
+  },
+  {
+    name: "Dalton Mangela",
+    savings: 111000
+  },
+  {
+    name: "Samson Mwaura",
+    savings: 95000
+  }
+];
